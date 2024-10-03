8fb0bd49b5cd31e25c7db73049b2714524b241f7
Logan-Delaney <logan_delaney@yahoo.com>
2024-10-02 20:53:22 -0400

    Add mouseover

--- etchSketch.js
+++ etchSketch.js
@@ -1,14 +1,17 @@
 const container = document.querySelector("#container");
-let side = 16;
-let fill = side * side
+let side = 100;
+let fill = side * side;
 
-for (i = 1; i <= fill; i++){
-let dimension = ((600 / side) - 2) + "px";
-// dimension += "px"
-const newdiv = document.createElement("div");
-container.appendChild(newdiv);
-newdiv.style.border = "1px solid black";
-newdiv.style.minHeight = dimension;
-newdiv.style.minWidth = dimension;
-newdiv.style.flexWrap = "wrap";
+for (i = 1; i <= fill; i++) {
+  let dimension = 600 / side - 2 + "px";
+  // dimension += "px"
+  const newdiv = document.createElement("div");
+  container.appendChild(newdiv).id = "newdiv";
+  newdiv.style.border = "1px solid black";
+  newdiv.style.minHeight = dimension;
+  newdiv.style.minWidth = dimension;
+  newdiv.style.flexWrap = "wrap";
+  newdiv.addEventListener("mouseover", (e) => {
+    newdiv.style.backgroundColor = "#000000";
+  });
 }
